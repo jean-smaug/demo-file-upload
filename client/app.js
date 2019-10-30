a972a429a352914bc9b9b2c2058a7a19c0781a72
jean-smaug <maximeblanc.dev@gmail.com>
2019-10-30 15:52:52 +0100

    refactor: remove useless headers

--- client/app.js
+++ client/app.js
@@ -19,14 +19,10 @@ submitButton.addEventListener("click", (e) => {
     e.preventDefault()
 
     const formData = new FormData()
-    const headers = new Headers();
-
     formData.append("file", fileInput.files[0])
-    headers.append("Content-Type", "multipart/form-data")
 
     fetch("http://localhost:4000/upload", { 
         method: "POST",
-        // headers,
         body: formData
     }).then(response => {
         console.log(response)
