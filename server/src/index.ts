15b42e2805ec680e7a6efd5557ab1a55ce60e7fa
jean-smaug <maximeblanc.dev@gmail.com>
2019-10-30 09:37:49 +0100

    feat: upload files to storage directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,27 @@
 import Koa from "koa";
 import Router from "@koa/router"
 import logger from "koa-logger"
+import fs from "fs";
+import path from "path"
 const multer = require('@koa/multer');
 
 const app: Koa = new Koa();
 const router: Router = new Router();
 const upload = multer();
 
+const storagePath = path.join(__dirname, "../storage")
+
+if(!fs.existsSync(storagePath)) {
+  fs.mkdirSync(storagePath)
+}
+
 app.use(logger())
 
 router.post("/upload", upload.single('profile'), (ctx: any) => {
+  const { originalname, buffer } = ctx.request.file
+
+  fs.writeFileSync(`${storagePath}/${originalname}`, buffer)
+
   ctx.status = 200
 })
 
