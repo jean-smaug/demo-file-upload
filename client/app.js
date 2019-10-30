4f3b1e71b196ec0193120de3e6cd6f9adc7f0252
maxime blanc <maximeblanc.dev@gmail.com>
2019-10-30 22:32:20 +0100

    style: format server

--- client/app.js
+++ client/app.js
@@ -2,37 +2,42 @@ const submitButton = document.getElementById("submitButton");
 const fileInput = document.getElementById("fileInput");
 const formGroup = document.getElementById("formGroup");
 const message = document.getElementById("message");
-const form = document.getElementById('form')
+const form = document.getElementById("form");
 
 function toggleFileInputStatus() {
-    if(formGroup.className.includes("FormGroup--WillDrop")) {
-        formGroup.className = "FormGroup"
+  if (formGroup.className.includes("FormGroup--WillDrop")) {
+    formGroup.className = "FormGroup";
 
-        return
-    }
+    return;
+  }
 
-    formGroup.className += " FormGroup--WillDrop"
+  formGroup.className += " FormGroup--WillDrop";
 }
 
-fileInput.addEventListener("dragenter", toggleFileInputStatus)
-fileInput.addEventListener("dragleave", toggleFileInputStatus)
+fileInput.addEventListener("dragenter", toggleFileInputStatus);
+fileInput.addEventListener("dragleave", toggleFileInputStatus);
 
-submitButton.addEventListener("click", async (e) => {
-    e.preventDefault()
-    if(!window.fetch || !window.FormData) {
-        alert('Respect toi')
-    }
+submitButton.addEventListener("click", async e => {
+  e.preventDefault();
+  if (!window.fetch || !window.FormData) {
+    alert("Respect toi");
+  }
 
-    const formData = new FormData(form)
-    
-    try {
-        await fetch("http://localhost:4000/upload", { 
-            method: "POST",
-            body: formData
-        })
+  const formData = new FormData(form);
 
-        message.innerText = "Fichier uploadé avec succès \\o/"
-    } catch (error) {
-        message.innerText = "C'est la cata, c'est la cata, c'est la catastrophe /o\\"
-    }
-})
+  try {
+    await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      body: formData
+    });
+
+    message.innerText = "Fichier uploadé avec succès \\o/";
+  } catch (error) {
+    message.innerText =
+      "C'est la cata, c'est la cata, c'est la catastrophe /o\\";
+  }
+
+  setTimeout(() => {
+    message.innerText = "";
+  }, 2000);
+});
