dc8fe9865076d434286dcc39485a802430a67dce
jean-smaug <maximeblanc.dev@gmail.com>
2019-10-31 10:02:28 +0100

    feat: improve php server

--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,12 @@
 <?php
-    if($_FILES["profile-picture"] > 0) {
-      echo "Fichier uploadé avec succès";
-    }
+    header("Access-Control-Allow-Origin: *");
     
+    $file = $_FILES["profile-picture"];
+    $fileUploaded = move_uploaded_file($file["tmp_name"], __DIR__ . "/../storage/" . $file["name"]);
+
+    if($fileUploaded === false) {
+        http_response_code(500);
+        return;
+    }
+
+    http_response_code(201);
